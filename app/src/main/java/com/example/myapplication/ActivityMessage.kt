0e0ff2ce690a2eb27493e0e669ee33fbb6cf2acf
amelh <kloyn321@gmail.com>
2023-04-17 20:50:11 +0300

    Add dependencies plugins/ ActiveMessage/

---
+++ app/src/main/java/com/example/myapplication/ActivityMessage.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.myapplication.utils.tools.helper.ShowDialogMessage
+
+class ActivityMessage :AppCompatActivity (){
+    private var isBackNoticed: Boolean = false
+
+    override fun onBackPressed() {
+        if(isTaskRoot && !isBackNoticed){
+            Toast.makeText(this,getString(R.string.Select_to_close),Toast.LENGTH_LONG)
+                .show()
+            isBackNoticed == true
+        }else{
+            super.onBackPressed()
+        }
+    }
+    private var messageReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            ShowDialogMessage(supportFragmentManager)
+                .setName(intent?.getStringExtra(NAME))
+                .show()
+        }
+    }
+    companion object{
+        var isActive: Boolean = false
+        const val BROADCAST_KEY = "alert"
+        const val NAME = "name"
+    }
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {}
+
+    override fun onResume() {
+        super.onResume()
+        isActive = true
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            messageReceiver, IntentFilter(BROADCAST_KEY)
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isActive = false
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver)
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/utils/tools/helper/ShowDialogMessage.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.utils.tools.helper
+
+import androidx.fragment.app.FragmentManager
+import com.example.myapplication.view.AlertMessageFragment
+
+class ShowDialogMessage(private var frm: FragmentManager?) {
+
+    var fragment: AlertMessageFragment = AlertMessageFragment()
+    fun show(){
+        if(frm!= null){
+            fragment.show(frm!!, "Message")
+        }
+    }
+
+    fun setName(name: String?): ShowDialogMessage{
+        fragment.name = name
+        return this
+    }
+}
